bec0b766c0ecc86b2b7f7c8acae8bb84f948e4de
killianjourneau <killianjourneau@free.fr>
2026-07-21 09:13:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Motus — service worker */
-const CACHE = "motus-v26";
-const V = "1.8.0";   // doit correspondre au ?v= des <script> de index.html
+const CACHE = "motus-v27";
+const V = "1.8.1";   // doit correspondre au ?v= des <script> de index.html
 const ASSETS = [
   "./",
   "./index.html",
