9eb3f993f9209d57789973b6bf9d555fd2aa3aa6
Nouvelle version <actions@github.com>
2026-09-03 14:00:43 +0000

    Version 1.51.1

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Mot en Six — service worker */
-const CACHE = "motus-v123";
-const V = "1.51.0";   // doit correspondre au ?v= des <script> de index.html
+const CACHE = "motus-v124";
+const V = "1.51.1";   // doit correspondre au ?v= des <script> de index.html
 const ASSETS = [
   "./",
   "./index.html",
